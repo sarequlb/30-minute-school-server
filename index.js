ff91f3e3273089908be6edf2b2d3ea0d43ad6fb8
Sarequl Basar <sarequl@proton.me>
2023-10-17 21:18:17 +0600

    category

--- index.js
+++ index.js
@@ -3,12 +3,29 @@ const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 5000;
+const categories = require('./data/courses-category.json')
+const courses = require('./data/courses.json')
 
 app.use(cors())
 
 app.get('/', (req,res) =>{
     res.send('School of Programming is running')
 })
+
+app.get('/category', (req,res) =>{
+    res.send(categories)
+})
+
+app.get('/category/:id', (req,res) =>{
+    const id = req.params.id;
+    const selectedCategory = categories.find(category => category.id === id)
+    res.send(selectedCategory)
+} )
+
+
+
+
+
 app.listen(port, () =>{
     console.log(`School of Programming running on ${port}`)
 })
