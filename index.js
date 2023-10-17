bdb3fa9b7365894894ab956bc24fd56cd88022e1
Sarequl Basar <sarequl@proton.me>
2023-10-17 09:24:42 +0600

    course-category-added

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+app.get('/', (req,res) =>{
+    res.send('School of Programming is running')
+})
+app.listen(port, () =>{
+    console.log(`School of Programming running on ${port}`)
+})
